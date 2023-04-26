f49fcf92d0939e80eb0852326f5369e2889a4839
SkySong4 <128451262+SkySong4@users.noreply.github.com>
2023-04-26 14:21:27 -0500

    Initial commit

---
+++ homework 5.py
@@ -0,0 +1,63 @@
+# PPHA 30537
+# Spring 2023
+# Homework 5
+
+# YOUR NAME HERE
+# YOUR GITHUB USER NAME HERE
+
+# Due date: Sunday April 30th before midnight
+# Write your answers in the space between the questions, and commit/push only
+# this file to your repo. Note that there can be a difference between giving a
+# "minimally" right answer, and a really good answer, so it can pay to put
+# thought into your work. Using functions for organization will be rewarded.
+
+##################
+
+# To answer these questions, you will continue from where you left off in
+# homework 4, by using the included file named hw4_data.csv.
+
+# Question 1a: Reshape the data from wide to long, using the wide_to_long function,
+# making sure you reset the index to the default values if any of your data is located 
+# in the index.  What happened to the POPCHANGE column, and why should it be dropped?
+
+# Question 1b: Repeat the reshaping using the melt method.  Clean up the result so
+# that it is the same as the result from 1a (without the POPCHANGE column).
+
+# Question 2: Open the state-visits.xlsx file, and fill in the VISITED column
+# with a dummy variable for whether you've visited a state or not.  If you
+# haven't been to many states, then filling in a random selection of them
+# is fine too.  Save your changes.  Then load the xlsx file as a dataframe in
+# Python, and merge the VISITED column into your original long-form population 
+# dataframe, only keeping values that appear in both dataframes.  Are any 
+# observations dropped from this?  Show code where you investigate your merge, 
+# and display any observations that weren't in both dataframes.
+
+# Question 3a: The file policy_uncertainty.xlsx contains monthly measures of 
+# economic policy uncertainty for each state.  The EPU_National column esimates
+# uncertainty from national sources, EPU_State from state, and EPU_Composite 
+# from both (EPU-N, EPU-S, EPU-C).  Load it as a dataframe, then calculate 
+# the mean EPU-C value for each state/year, leaving only columns for state, 
+# year, and EPU_Composite, with each row being a unique state-year combination.
+
+# Question 3b) Reshape the EPU data into wide format so that each row is unique 
+# by state, and the columns represent the EPU-C values for the years 2022, 
+# 2021, and 2020. 
+
+# Question 3c) Finally, merge this data into your merged data from question 2, 
+# making sure the merge does what you expect.
+
+# Question 4: Using groupby on the VISITED column in the dataframe resulting 
+# from the previous question, answer the following questions and show how you  
+# calculated them: a) what is the single smallest state by 2022 population  
+# that you have visited, and not visited?  b) what are the three largest states  
+# by 2022 population you have visited, and the three largest states by 2022 
+# population you have not visited? c) do states you have visited or states you  
+# have not visited have a higher average EPU-C value in 2022?
+
+# Question 5: Transforming data to have mean zero and unit standard deviation
+# is often called "standardization", or a "zscore".  The basic formula to 
+# apply to any given value is: (value - mean) / std
+# Return to the long-form EPU data you created in step 3a and then, using groupby
+# and a function you write, transform the data so that the values for EPU-C
+# have mean zero and unit standard deviation for each state.  Add these values
+# to a new column named EPU_C_zscore.
